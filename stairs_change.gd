89a92a90678cfad549f34c19b0847c4dc3b7aeb2
Oscariolo <oscariolopinop21@gmail.com>
2025-03-12 21:10:16 -0500

    works sort of well giving a base stairs area detection

--- stairs_change.gd
+++ stairs_change.gd
@@ -6,15 +6,11 @@ var on_upper_floor = false
 
 
 func _on_transitioning_stairs_body_entered(body: Node2D) -> void:
-	transitioning = true
-	on_upper_floor = false
 	$"../FloorController".set_player_floor(2)
 
 
-func _on_up_floor_body_entered(body: Node2D) -> void:
-	on_upper_floor = true
-
-
 func _on_transitioning_stairs_body_exited(body: Node2D) -> void:
-	if !on_upper_floor:
+	if body.velocity.y >= 0:
 		$"../FloorController".set_player_floor(1)
+	if body.velocity.y < 0:
+		$"../FloorController".set_player_floor(2)
